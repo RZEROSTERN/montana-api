1fa94ca960126e0ba85185dd591d6e05f2034d87
Marco Ramirez <marco.ramirez@rzerocorp.com.mx>
2023-08-18 22:50:22 -0600

    Attempting new focus for Github Actions

--- app/Http/Controllers/Api/Users/UserController.php
+++ app/Http/Controllers/Api/Users/UserController.php
@@ -179,6 +179,8 @@ class UserController extends Controller
             'scope' => '*',
         ];
 
+        var_dump($body);
+
         if (App::runningUnitTests()) {
             // $body['client_id'] = "5";
             // $body['client_secret'] = "T7gtfKg2YvAXaPmFimlY68ktHs5lGxWoDiYbDIvX";
